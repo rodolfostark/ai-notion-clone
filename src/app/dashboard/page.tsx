c34750a4a138d3fc8de2144f079ffd6c6ecd7734
rodolfostark <rodolfojonatas@outlook.com>
2023-12-03 21:12:56 -0300

    feat: create dashboard page

---
+++ src/app/dashboard/page.tsx
@@ -0,0 +1,10 @@
+export default function Dashboard() {
+    return (
+        <div className="grainy min-h-screen">
+            <div className="max-w-7xl mx-auto p-10">
+
+            </div>
+
+        </div>
+    );
+}
